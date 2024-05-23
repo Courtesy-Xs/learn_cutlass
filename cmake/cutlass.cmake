d963b791c0cb861de13e4659cd9ac0161261cf58
Courtesy-Xs <xs1580802568@gmail.com>
2024-05-23 10:50:20 +0000

    add cutlass + cmake

---
+++ cmake/cutlass.cmake
@@ -0,0 +1,10 @@
+include(FetchContent)
+
+FetchContent_Declare(
+    extern_cutlass 
+    GIT_REPOSITORY https://github.com/NVIDIA/cutlass.git 
+    GIT_TAG "main"
+    SOURCE_DIR ${CMAKE_SOURCE_DIR}/third_party/cutlass/
+)
+FetchContent_MakeAvailable(extern_cutlass)
+# include_directories("${third_party}/cutlass/include/")
---
+++ test_cutlass.cc
@@ -0,0 +1,8 @@
+#include <cute/numeric/int.hpp>
+
+
+int main()
+{
+    cute::int2_t i;
+    return 0;
+}
