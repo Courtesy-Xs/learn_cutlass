7e87bdda8d03625df85aed7427468a1bf19adf34
Courtesy-Xs <xs1580802568@gmail.com>
2024-05-24 08:26:02 +0000

    none

--- cmake/cutlass.cmake
+++ cmake/cutlass.cmake
@@ -1,3 +1,26 @@
+# include(ExternalProject)
+
+# set(CUTLASS_PREFIX_DIR ${THIRD_PARTY_PATH}/cutlass)
+# set(CUTLASS_SOURCE_DIR ${THIRD_PARTY_PATH}/cutlass)
+# set(CUTLASS_GIT_REPOSITORY https://github.com/NVIDIA/cutlass.git)
+# set(CUTLASS_GIT_TAG "main")
+
+# ExternalProject_Add(
+#   extern_cutlass
+#   GIT_REPOSITORY https://github.com/NVIDIA/cutlass.git
+#   GIT_TAG ${CUTLASS_GIT_TAG}
+#   PREFIX ${CUTLASS_PREFIX_DIR}
+#   SOURCE_DIR ${CUTLASS_SOURCE_DIR}
+#   BINARY_DIR ${CMAKE_BINARY_DIR}
+#   UPDATE_COMMAND ""
+#   CONFIGURE_COMMAND ""
+#   BUILD_COMMAND ""
+#   INSTALL_COMMAND ""
+#   TEST_COMMAND "")
+
+# include_directories("${THIRD_PARTY_PATH}/cutlass/include/")
+
+
 include(FetchContent)
 
 FetchContent_Declare(
@@ -6,5 +29,5 @@ FetchContent_Declare(
     GIT_TAG "main"
     SOURCE_DIR ${CMAKE_SOURCE_DIR}/third_party/cutlass/
 )
+
 FetchContent_MakeAvailable(extern_cutlass)
-# include_directories("${third_party}/cutlass/include/")
---
+++ test_cutlass.cu
@@ -0,0 +1,8 @@
+// #include "cute/numeric/int.hpp"
+#include "cutlass/array.h"
+
+int main()
+{
+
+    return 0;
+}
